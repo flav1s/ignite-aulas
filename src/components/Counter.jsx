f7ed602c74c18e627084d4effefc4355fae40202
Flávia Rocha <flaviaccruzr@gmail.com>
2021-07-09 14:27:47 -0300

    chore: state concept

---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div>
+      <h2>{counter}</h2>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
