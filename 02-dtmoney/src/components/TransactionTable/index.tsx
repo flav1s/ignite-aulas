f0e3dcab0dc97b99cfe31bd285418911d380afed
Flávia Rocha <flaviaccruzr@gmail.com>
2021-07-14 08:53:32 -0300

    feat: create API service with axios

--- 02-dtmoney/src/components/TransactionTable/index.tsx
+++ 02-dtmoney/src/components/TransactionTable/index.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export const TransactionTable = () => {
   useEffect(() => {
-    fetch("http://localhost:3001/api/transactions")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    api.get("transactions").then((response) => console.log(response.data));
   }, []);
 
   return (
