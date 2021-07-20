fc2a35474b247b0caa3b64c7ea592356a715b52a
Flávia Rocha <flaviaccruzr@gmail.com>
2021-07-20 17:04:01 -0300

    styles: add styles to home

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,12 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏻 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   );
 }
