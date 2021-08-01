27e06200e215006d9eece0f8bcfbb5bfcfcd89de
Flávia Rocha <flaviaccruzr@gmail.com>
2021-07-31 21:28:16 -0300

    refactor: impl responsive user list

--- 04-dashgo/src/pages/dashboard.tsx
+++ 04-dashgo/src/pages/dashboard.tsx
@@ -68,7 +68,7 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" aling="flex=start">
-          <Box p="8" bgColor="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bgColor="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
