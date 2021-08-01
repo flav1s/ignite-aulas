7017aec53e8d8897a30cb6f03fa6588132ebc959
Flávia Rocha <flaviaccruzr@gmail.com>
2021-08-01 13:16:12 -0300

    chore: add validation schema to login form

--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import { Input } from "../components/Form/Input";
 
@@ -8,11 +10,19 @@ type SignInFormData = {
   password: string;
 };
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+  password: yup.string().required("Senha obrigatória"),
+});
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+  const { errors } = formState;
+  console.log(errors);
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
-    await new Promise((resolve, reject) => setTimeout(resolve, 2000));
     console.log(values);
   };
 
@@ -33,6 +43,7 @@ export default function SignIn() {
             name="email"
             label="E-mail"
             type="email"
+            error={errors.email}
             {...register("email")}
           />
 
@@ -40,6 +51,7 @@ export default function SignIn() {
             name="password"
             label="Password"
             type="password"
+            error={errors.password}
             {...register("password")}
           />
         </Stack>
