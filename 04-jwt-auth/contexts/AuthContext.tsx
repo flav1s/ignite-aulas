913ff3403fe3b34d52ec52a489ecd50e27be020b
Flávia Rocha <flaviaccruzr@gmail.com>
2021-08-03 14:37:40 -0300

    WIP: sign in with api

--- 04-jwt-auth/contexts/AuthContext.tsx
+++ 04-jwt-auth/contexts/AuthContext.tsx
@@ -1,5 +1,12 @@
-import { createContext, ReactNode } from "react";
+import Router from "next/router";
+import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
@@ -8,6 +15,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  user: User | undefined;
   isAuthenticated: boolean;
 };
 
@@ -18,20 +26,29 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+
+  const isAuthenticated = !!user;
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post("sessions", { email, password });
+      const { token, refreshToken, permissions, roles } = response.data;
 
-      console.log(response.data);
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push("/dashboard");
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
