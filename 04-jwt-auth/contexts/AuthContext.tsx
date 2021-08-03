4c77f1987a0dd45f27783f0cc6d3ccf994e95fc1
Flávia Rocha <flaviaccruzr@gmail.com>
2021-08-03 15:12:04 -0300

    chore: add headers to api

--- 04-jwt-auth/contexts/AuthContext.tsx
+++ 04-jwt-auth/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import Router from "next/router";
-import { setCookie } from "nookies";
+import { parseCookies, setCookie } from "nookies";
 
 import { api } from "../services/api";
 
@@ -29,8 +29,20 @@ export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
-
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "@nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({ email, permissions, roles });
+      });
+    }
+  }),
+    [];
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -51,6 +63,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles,
       });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
 
       Router.push("/dashboard");
     } catch (error) {
