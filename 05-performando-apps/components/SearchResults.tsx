eda7ac7dddc4b4d3a7e465a39168053c34313ad3
Flávia Rocha <flaviaccruzr@gmail.com>
2021-08-10 09:04:20 -0300

    chore: optimize total price calc with useMemo

--- 05-performando-apps/components/SearchResults.tsx
+++ 05-performando-apps/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -6,8 +6,15 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
