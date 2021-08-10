39568bf9b9d0f3d24353834d245b9fc90267a8cd
Flávia Rocha <flaviaccruzr@gmail.com>
2021-08-10 09:57:20 -0300

    chore: add virtualized lists to improve projects performance

--- 05-performando-apps/components/SearchResults.tsx
+++ 05-performando-apps/components/SearchResults.tsx
@@ -1,4 +1,6 @@
-import React, { useMemo } from "react";
+// import React, { useMemo } from "react";
+
+import { List, ListRowRenderer } from "react-virtualized";
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -22,17 +24,28 @@ export function SearchResults({
   //     return total + product.price;
   //   }, 0);
   // }, [results]);
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          onAddToWishlist={onAddToWishlist}
+          product={results[index]}
+        />
+      </div>
+    );
+  };
 
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map((product) => (
-        <ProductItem
-          key={product.id}
-          onAddToWishlist={onAddToWishlist}
-          product={product}
-        />
-      ))}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
